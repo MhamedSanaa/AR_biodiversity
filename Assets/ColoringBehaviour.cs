4ff3ed02cc1a707483ce5d9e63f902104aa4de39
mhamedsanaa <mhamed.sanaa6@gmail.com>
2023-10-23 20:31:26 +0100

    bioversity v0.3a

--- Assets/ColoringBehaviour.cs
+++ Assets/ColoringBehaviour.cs
@@ -4,66 +4,91 @@ using UnityEngine;
 
 public class ColoringBehaviour : MonoBehaviour
 {
-    public Camera paintingCamera;
-    public GameObject objectToPaint;
+    public Camera mainCamera;
     public Texture2D brushTexture;
-    public Color paintColor = Color.red;
-    public float brushSize = 0.1f;
-
-    private RaycastHit hit;
-    private Renderer objectRenderer;
+    public float brushSize = 1f;
+    public Color brushColor = Color.red;
+    private LineRenderer lineRenderer;
+    private Renderer rend;
     private Texture2D canvasTexture;
-    private bool isPainting = false;
-
     void Start()
     {
-        objectRenderer = objectToPaint.GetComponent<Renderer>();
-        if (objectRenderer)
-        {
-            canvasTexture = new Texture2D(1024, 1024);
-            objectRenderer.material.mainTexture = canvasTexture;
-        }
+        Application.targetFrameRate = 10;
+        rend = GetComponent<Renderer>();
+        canvasTexture = new Texture2D(1024, 1024);
+        canvasTexture.wrapMode = TextureWrapMode.Clamp;
+        rend.material.mainTexture = canvasTexture;
+
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.startColor = Color.green;
+        lineRenderer.endColor = Color.green;
     }
 
     void Update()
     {
+        //Debug.Log("IsMouseInput");
+        //Debug.Log(Input.GetMouseButton(0));
         if (Input.GetMouseButton(0))
         {
-            isPainting = true;
-            Ray ray = paintingCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
             {
-                Paint();
-            }
-        }
-        else
-        {
-            isPainting = false;
-        }
-    }
+                //Debug.Log("touched obejct");
+                //Debug.Log(hitInfo.transform.name);
 
-    void Paint()
-    {
-        if (isPainting && objectRenderer != null)
-        {
-            Vector2 pixelUV = hit.textureCoord;
-            pixelUV.x *= canvasTexture.width;
-            pixelUV.y *= canvasTexture.height;
-
-            int posX = (int)pixelUV.x;
-            int posY = (int)pixelUV.y;
-
-            for (int x = posX - (int)(brushSize * canvasTexture.width); x < posX + (int)(brushSize * canvasTexture.width); x++)
-            {
-                for (int y = posY - (int)(brushSize * canvasTexture.height); y < posY + (int)(brushSize * canvasTexture.height); y++)
+                if (hitInfo.transform == transform)
                 {
-                    if (x >= 0 && x < canvasTexture.width && y >= 0 && y < canvasTexture.height)
-                    {
-                        canvasTexture.SetPixel(x, y, paintColor);
-                    }
+                    Debug.Log("hit obejct-----------------------------");
+                    Debug.Log(hitInfo.transform.name);
+                    Debug.Log("obejct-----------------------------");
+                    Debug.Log(transform.name);
+                    Paint(hitInfo.textureCoord);
                 }
             }
-            canvasTexture.Apply();
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
+            lineRenderer.SetPosition(0, ray.origin);
+            lineRenderer.SetPosition(1, ray.origin + ray.direction * 100);
         }
+       /* Debug.Log("IsTouchInput");
+        Debug.Log(Input.GetTouch(0).phase == TouchPhase.Began);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position); // Cast a ray from the touch position
+
+            RaycastHit hitInfo; // Variable to hold the raycast hit information
+
+            if (Physics.Raycast(ray, out hitInfo)) // Check if the ray hits any collider
+            {
+                if (hitInfo.transform == transform)
+                {
+                    Paint(hitInfo.textureCoord);
+                }
+            }
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.green);
+            lineRenderer.SetPosition(0, ray.origin);
+            lineRenderer.SetPosition(1, ray.origin + ray.direction * 100);
+        }*/
+    }
+
+    void Paint(Vector2 uv)
+    {
+        int x = (int)(uv.x * canvasTexture.width);
+        int y = (int)(uv.y * canvasTexture.height);
+
+        for (int i = -Mathf.RoundToInt(brushSize); i < Mathf.RoundToInt(brushSize); i++)
+        {
+            for (int j = -Mathf.RoundToInt(brushSize); j < Mathf.RoundToInt(brushSize); j++)
+            {
+                if (x + i >= 0 && x + i < canvasTexture.width && y + j >= 0 && y + j < canvasTexture.height)
+                {
+                    canvasTexture.SetPixel(x + i, y + j, brushColor);
+                }
+            }
+        }
+        canvasTexture.Apply();
     }
 }
